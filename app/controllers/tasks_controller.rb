9f1185d5bfd79766a700d17866ef0d60c516238f
patty <pattycheng2008@gmail.com>
2022-03-02 10:46:25 +0800

    add task CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_param)
+    if @task.save
+      redirect_to tasks_path, notice: "任務新增成功"
+    else
+      render :new, notice: "任務新增失敗"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_param)
+      redirect_to tasks_path, notice: "任務已修改成功"
+    else
+      render :edit, notice: "任務修改失敗"
+    end
+  end
+
+  def destroy
+    if @task
+      @task.destroy
+    end
+    redirect_to tasks_path, notice: "已刪除任務"
+  end
+
+  private
+  def task_param
+    params.require(:task).permit(:name, :content)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+end
